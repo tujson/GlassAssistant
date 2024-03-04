486a66cd5b026694d531a321c24afa87b20ea91d
Jason Tu <title.kubo@gmail.com>
2024-03-03 20:30:21 -0800

    Incremented app version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "dev.synople.glassassistant"
         minSdk = 27
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
